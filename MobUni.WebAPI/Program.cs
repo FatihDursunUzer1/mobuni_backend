43a851d56c630285422258996ec1173e10424717
FATİH DURSUN ÜZER <18120205041@ismu.edu.tr>
2022-06-12 13:44:03 +0300

    -test

--- MobUni.WebAPI/Program.cs
+++ MobUni.WebAPI/Program.cs
@@ -44,11 +44,10 @@ builder.Services.AddMvc().AddFluentValidation(fv =>
 builder.Services.AddControllers(options => options.Filters.Add<ActionFilter>());
 
 //builder.Services.AddDbContext<MobUniDbContext>(options=> options.UseSqlServer("Data Source=mobuni.c9uwcgm4xelz.us-east-2.rds.amazonaws.com,1433;Initial Catalog=MobUni;User ID=admin;Password=oz15ar47uz28;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
-
+builder.Services.AddDbContext<MobUniDbContext>(optionsBuilder => optionsBuilder.UseLazyLoadingProxies().
+            UseSqlServer("Data Source=mobuni.c9uwcgm4xelz.us-east-2.rds.amazonaws.com,1433;Initial Catalog=MobUni;User ID=admin;Password=oz15ar47uz28;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"), ServiceLifetime.Transient, ServiceLifetime.Transient);
 builder.Services.AddSingleton<IStorage, AzureStorage >();
 builder.Services.AddHostedService<ActivityTimeOutService>();
-builder.Services.AddDbContext<MobUniDbContext>(optionsBuilder=>optionsBuilder.UseLazyLoadingProxies().
-            UseSqlServer("Data Source=mobuni.c9uwcgm4xelz.us-east-2.rds.amazonaws.com,1433;Initial Catalog=MobUni;User ID=admin;Password=oz15ar47uz28;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"),ServiceLifetime.Transient,ServiceLifetime.Transient);
 builder.Services.AddTransient<IActivityService, ActivityService>();
 builder.Services.AddTransient<IQuestionService, QuestionService>();
 builder.Services.AddTransient<IQuestionRepository,QuestionRepository>();
